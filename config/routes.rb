ef8fb58b9a6155c9dfdd87e61cb2e634b5a72868
Fernando Juarez <fhereduardo90@gmail.com>
2017-06-18 20:26:40 -0600

    Add doorkeeper routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, skip: :all
+
+  scope 'v1' do
+    use_doorkeeper do
+      skip_controllers :applications, :authorized_applications, :authorizations
+    end
+  end
+
   mount Agromotivapp::API => '/'
 end
