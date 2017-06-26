db98745813b15c6a828fbcc6a905257ad08bf097
Fernando Juarez <fhereduardo90@gmail.com>
2017-06-25 23:07:31 -0600

    Do not allow more than one asset in a deleted state per Attachable Type

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,8 @@
 class Asset < ApplicationRecord
   belongs_to :attachable, polymorphic: true
 
+  UNIQUE_ATTACHABLE_CLASSES = %w(User Seller Category).freeze
+
   has_attached_file :image, path: ':class/:attachable_type/:attachable_id/:style/:id/:filename'
   validates_attachment :image,
                        content_type: { content_type: ['image/jpeg', 'image/png'] },
@@ -8,7 +10,7 @@ class Asset < ApplicationRecord
                        presence: true
 
   validates :attachable_id, uniqueness: {
-    conditions: proc { where('attachable_type = ? AND deleted = ?', User.to_s, false) }
+    conditions: proc { where('attachable_type IN (?) AND deleted = ?', UNIQUE_ATTACHABLE_CLASSES, false) }
   }, unless: proc { |a| a.deleted == true }
 
   Paperclip.interpolates :attachable_type do |attachment, style|
